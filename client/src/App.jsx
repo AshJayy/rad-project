043e6e8cffff84427e1da12036fd4a60f71274ff
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-06-10 14:00:37 +0530

    creating brower routes & flowbite react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import FreeTrial from './pages/FreeTrial'
+import Exam from './pages/Exam'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Subscribe from './pages/Subscribe'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <div>
-      Exam Ease
-    </div>
+    <BrowserRouter>
+      <Header />
+        <Routes>
+        <Route path="Home" element={<Home />} />
+        <Route path="FreeTrial" element={<FreeTrial />} />
+        <Route path="Exam" element={<Exam />} />
+        <Route path="SignIn" element={<SignIn />} />
+        <Route path="SignUp" element={<SignUp />} />
+        <Route path="Subscribe" element={<Subscribe />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
