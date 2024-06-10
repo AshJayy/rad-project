f1133371fc9bde91dbf376bd508f051e07681edf
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-06-10 14:10:56 +0530

    correcting path cases

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,12 +13,12 @@ function App() {
     <BrowserRouter>
       <Header />
         <Routes>
-        <Route path="Home" element={<Home />} />
-        <Route path="FreeTrial" element={<FreeTrial />} />
-        <Route path="Exam" element={<Exam />} />
-        <Route path="SignIn" element={<SignIn />} />
-        <Route path="SignUp" element={<SignUp />} />
-        <Route path="Subscribe" element={<Subscribe />} />
+        <Route path="home" element={<Home />} />
+        <Route path="freetrial" element={<FreeTrial />} />
+        <Route path="exam" element={<Exam />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="subscribe" element={<Subscribe />} />
         </Routes>
     </BrowserRouter>
   )
