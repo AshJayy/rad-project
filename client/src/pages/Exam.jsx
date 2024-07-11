8e5309af93dc446f9317e3a4f625fa97301001e1
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-07-11 21:50:39 +0530

    Create Exam UI

--- client/src/pages/Exam.jsx
+++ client/src/pages/Exam.jsx
@@ -1,15 +1,18 @@
-import { Button, Label, List, Radio, Spinner } from "flowbite-react";
+import { Alert, Button, Label, List, Radio, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { AiFillClockCircle } from "react-icons/ai";
-import { HiChevronRight } from "react-icons/hi";
+import { HiChevronRight, HiOutlinePencilAlt, HiOutlinePencil } from "react-icons/hi";
+import { FaCheck, FaTimes } from "react-icons/fa";
 
 export default function Exam() {
   const [questions, setQuestions] = useState([]);
   const [questionNo, setQuestionNo] = useState(0)
   const [completed, setCompleted] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [marks, setMarks] = useState(0);
+  
 
-  useEffect(() => {
+  useEffect(() => { //get questions from the server
     try {
       const fetchQuestions = async () => {
         setLoading(true);
@@ -35,7 +38,21 @@ export default function Exam() {
     }
   }, [])
 
-  const handleAnswers = (qNo, selectedIndex) => {
+  useEffect(() => { // calculate marks got from the quizz
+    const getMarks = () => {
+      const marks = questions.reduce((acc, question) => {
+        if(question.choice === question.correctAnswer){
+          return acc + 1
+        }
+        return acc
+      }, 0)
+      return marks
+    }
+    const marks = getMarks() / questions.length * 100;
+    setMarks(marks.toFixed(0))
+  }, [completed])
+
+  const handleAnswers = (qNo, selectedIndex) => { //update state with selected answers
     const updatedQuestions = questions.map((question, index) => {
       if (index === qNo) {
         if(selectedIndex == question.choice){
@@ -52,15 +69,80 @@ export default function Exam() {
     setCompleted(true);
   }
 
-  if(completed){
+  if(completed){ // page after submitting answers
     return (
       <div>
-        completed
+        <div className="bg-mid-blue p-8 text-sm font-semibold">
+        <h3 className="text-white text-2xl pl-[14vw]">Your Score : {marks}%</h3>
+        <p className="text-white opacity-50 pl-[14vw]">completed</p>
       </div>
+      <div className="mt-10 flex flex-col justify-center items-center gap-12">
+        {questions.map((question, index) => {
+          return (
+            <>
+            <div key={index} className="max-w-6xl w-full py-8 px-12 text-sm rounded-3xl drop-shadow-md bg-light-blue">
+              <p className="p-2">{index + 1}. {question.content}</p>
+              <div>
+                {question.options.map((option, index) => (
+                  <div className="ml-10 p-1" key={index}>
+                    <Radio
+                      id={`op${index}`}
+                      readOnly
+                      checked={index == question.choice}
+                      className={`${index === question.choice ? ( question.choice === question.correctAnswer ? 'text-green-500' : 'text-red-500'  ) : 'bg-light-blue' }`}
+                    />
+                    <Label 
+                      htmlFor={`op${index}`} 
+                      className={`px-2 ${index === question.choice ? ( question.choice === question.correctAnswer ? 'text-green-500' : 'text-red-500'  ) : 'bg-light-blue' }`}>
+                        {option}
+                    </Label>
+                  </div>
+                ))}
+              </div>
+            </div>
+            {question.choice === question.correctAnswer && (
+              <Alert color="success" className="w-full max-w-6xl mt-[-25px] drop-shadow-md">
+                <div className="flex flex-row">
+                  <FaCheck className="mr-2 mt-[3px]" />
+                  <p>Correct Answer</p>
+                </div>
+              </Alert>
+            )}
+            {question.choice !== question.correctAnswer &&  (
+              <div className="w-full max-w-6xl">
+                <Alert color="failure" className="mt-[-30px] drop-shadow-md flex flex-row">
+                  <div className="flex flex-row">
+                    <FaTimes className="mr-2 mt-[3px]" />
+                    <p>Incorrect Answer</p>                
+                  </div>
+                </Alert>
+                <Alert className="bg-[#e6ddda] mt-4 w-full max-w-6xl drop-shadow-md flex flex-row">
+                  <div className="flex flex-row w-full justify-between max-w-6xl">
+                    <div className="flex - flex-row">
+                      <HiOutlinePencil className="mt-[2px] mr-2"/>
+                      Answer : {question.options[question.correctAnswer]} 
+                    </div>
+                    {/* <button className=""> show reason </button>    */}
+                  </div>
+                  <p className="flex flex-row max-w-6xl w-[70vw]">
+                    <HiOutlinePencilAlt className="mt-[2px] mr-2"/>
+                    <div className="">
+                      Justification : {question.justification}
+                    </div>
+                  </p>
+                </Alert>
+              </div>
+            )}
+            </>  
+          )
+        })}
+      </div>    
+      </div>
+    
     )
   }
 
-  return (
+  return ( // page before submitting answers
     <div className="min-h-screen">
       <div className="bg-mid-blue p-8 text-sm font-semibold">
         <h3 className="text-white">This free trial contains 10 questions.   Each question has 5 choices as answers.</h3>
@@ -75,9 +157,8 @@ export default function Exam() {
           {!loading &&
             <div className="w-full flex justify-end">
               {questions.length > 0 && questions.map((question, index) => (
-                <div className="flex items-center">
-                  <button 
-                    key={index} 
+                <div className="flex items-center" key={index}>
+                  <button  
                     onClick={() => setQuestionNo(index)}
                     className={`w-10 h-10 rounded-full ${question.choice > -1 ? 'bg-mid-blue text-white' : 'bg-light-blue' } transition-all`}>
                       {index + 1}
@@ -93,10 +174,10 @@ export default function Exam() {
             <p className="p-2">{questionNo + 1}. {questions[questionNo].content}</p>
             <div>
               {questions[questionNo].options.map((option, index) => (
-                <div className="ml-10 p-1">
+                <div className="ml-10 p-1" key={index}>
                   <Radio
                     id={`op${index}`}
-                    key={index}
+                    readOnly
                     checked={index == questions[questionNo].choice}
                     onClick={() => handleAnswers(questionNo, index)}
                   />
@@ -106,8 +187,8 @@ export default function Exam() {
             </div>
           </div>
         ) : (
-          <div>
-            <Spinner />
+          <div className="w-12 h-12">
+            <Spinner className="text-blue-500"/>
           </div>
         )}
         {questionNo <= questions.length - 2 ? (
@@ -122,6 +203,7 @@ export default function Exam() {
             </span>
           </Button>
         ) : (
+          (questionNo == questions.length - 1 ) && (
           <Button
             className="self-end px-4 bg-mid-blue"
             pill
@@ -131,6 +213,7 @@ export default function Exam() {
               Submit Answers
             </span>
           </Button>
+          )
         )}
       </main>
     </div>
