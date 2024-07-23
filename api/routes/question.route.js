6c468dc6755b96d4d58491859e1842f67af1b913
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-07-23 11:38:50 +0530

    Remove getQuestions Api made to bulk enter

--- api/routes/question.route.js
+++ api/routes/question.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { createQuestion, getFreeTrial, getQuestions, editQuestion, deleteQuestion, createQuestions } from '../controllers/question.controller.js';
+import { createQuestion, getFreeTrial, getQuestions, editQuestion, deleteQuestion, } from '../controllers/question.controller.js';
 import { get } from 'mongoose';
 
 const router = express.Router();
