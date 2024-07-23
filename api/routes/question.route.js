ef0dd35977feab29746333b069c9bad84e4f2f37
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-07-23 10:33:20 +0530

    create API to insert questions for better purposes

--- api/routes/question.route.js
+++ api/routes/question.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { createQuestion, getFreeTrial, getQuestions, editQuestion, deleteQuestion } from '../controllers/question.controller.js';
+import { createQuestion, getFreeTrial, getQuestions, editQuestion, deleteQuestion, createQuestions } from '../controllers/question.controller.js';
 import { get } from 'mongoose';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createQuestion);
+router.post('/createBulk', verifyToken, createQuestions);//to insert questions in bulk for testing purposes
 router.get('/freetrial', getFreeTrial);
 router.get('/getquestions', getQuestions);
 router.put('/editquestion', editQuestion); //verifyToken should be added later
