76dcaa4e34d50af3721e44c8220962b37c5e4e5f
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-08-11 09:14:45 +0530

    wip: add exam controller and fetch in freetrial

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import questionRoutes from './routes/question.route.js'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js';
+import examRoutes from './routes/exam.route.js';
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.listen(3000, () => {
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/question', questionRoutes);
+app.use('/api/exam', examRoutes);
 
 
 
