c901f4ae81c63968a429ab52559f064acf4b6fdb
AshJayy <ashwinie.ja@gmail.com>
2024-08-12 10:43:26 +0530

    wip: added getQuestion route

--- api/routes/exam.route.js
+++ api/routes/exam.route.js
@@ -1,9 +1,10 @@
 import express  from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { addExam }  from "../controllers/exam.controller.js";
+import { addExam, getUserExams }  from "../controllers/exam.controller.js";
 
 const router = express.Router();
 
 router.post('/addexam', addExam);
+router.get('/getuserexams', verifyToken, getUserExams);
 
 export default  router;
