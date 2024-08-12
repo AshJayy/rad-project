e8f41ab922e2afab677288c54890403e9e6d8dbf
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-08-13 00:00:48 +0530

    feat: complete view history list

--- client/src/components/DashExamHistory.jsx
+++ client/src/components/DashExamHistory.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Button, Spinner } from "flowbite-react";
 import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
+import { useSelector } from "react-redux";
 
 export default function DashExamHistory() {
   const [openedExams, setOpenedExams] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [examinations, setExaminations] = useState([])
-
+  const [examinations, setExaminations] = useState([]);
+  const user = useSelector((state) => state.user.currentUser);
+  // console.log(examinations)
   useEffect(() => {
     const getExams = async () => {
         try {
             setLoading(true);
-            const res = await fetch('/api/exam/getExams');
+            const res = await fetch(`/api/exam/getuserexams?userID=${user._id}`);
 
             if (!res.ok) {
                 setLoading(false);
@@ -28,7 +30,7 @@ export default function DashExamHistory() {
     };
 
     getExams();
-}, []); 
+}, [user?._id]); 
 
   // const examinations = [//sample exams
   //   {
@@ -37,7 +39,7 @@ export default function DashExamHistory() {
   //     Score: "80",
   //     NoOfCorrect: "8/10",
   //     date: "2024/05/03",
-  //     timetaken: "30 mins 03 sec",
+  //     timetaken: 120,
   //   },
   //   {
   //     _id: "2",
@@ -45,7 +47,7 @@ export default function DashExamHistory() {
   //     Score: "90",
   //     NoOfCorrect: "9/10",
   //     date: "2024/05/03",
-  //     timetaken: "30 mins 03 sec",
+  //     timetaken: 120,
   //   },
   //   {
   //     _id: "3",
@@ -53,7 +55,7 @@ export default function DashExamHistory() {
   //     Score: "100",
   //     NoOfCorrect: "10/10",
   //     date: "2024/05/03",
-  //     timetaken: "30 mins 03 sec",
+  //     timetaken: 90,
   //   },
   //   {
   //     _id: "4",
@@ -61,7 +63,7 @@ export default function DashExamHistory() {
   //     Score: "70",
   //     NoOfCorrect: "7/10",
   //     date: "2024/05/03",
-  //     timetaken: "30 mins 03 sec",
+  //     timetaken: 45,
   //   },
   // ];
   
@@ -99,7 +101,7 @@ export default function DashExamHistory() {
             className='flex flex-row justify-between w-full font-semibold text-xl cursor-pointer'
             onClick={() => updateOpenedExams(exam._id)}
           >
-            <div>{exam.name}</div>
+            <div>Examination {exam.examNo}</div>
             <div className='scale-[200%] mt-1'>
               {openedExams.includes(exam._id) ? (
                 <RiArrowDropUpLine />
@@ -115,23 +117,27 @@ export default function DashExamHistory() {
           >
             <div className='mt-3 mb-5 flex  flex-col gap-1'>
               <h1>
-                Score: <span className='text-gray-500 ml-5'>{exam.Score}%</span>
+                Score: <span className='text-gray-500 ml-5'>{exam.totalMarks}%</span>
               </h1>
               <h1>
                 No of correct answers:{" "}
-                <span className='text-gray-500 ml-5'>{exam.NoOfCorrect}</span>
+                <span className='text-gray-500 ml-5'>{exam.questions.length * exam.totalMarks / 100} / {exam.questions.length}</span>
               </h1>
               <h1>
                 Date and time:{" "}
-                <span className='text-gray-500 ml-5'>{exam.date}</span>
+                <span className='text-gray-500 ml-5'>
+                  {`${new Date(exam.createdAt).getDate().toString().padStart(2, '0')}/${(new Date(exam.createdAt).getMonth() + 1).toString().padStart(2, '0')}/${new Date(exam.createdAt).getFullYear()} at 
+                    ${new Date(exam.createdAt).getHours().toString().padStart(2, '0')}:${new Date(exam.createdAt).getMinutes().toString().padStart(2, '0')}`}
+                </span>
+
               </h1>
               <h1>
                 Time Taken:{" "}
-                <span className='text-gray-500 ml-5'>{exam.timetaken}</span>
+                <span className='text-gray-500 ml-5'>{exam.timeTaken ? (exam.timeTaken / 60).toFixed(0) : '2' } hours {exam.timeTaken && exam.timeTaken % 60 !== 0 ? `${exam.timetaken % 60} minutes` : ''} </span>
               </h1>
             </div>
             <div className='flex justify-end'>
-              <button className='rounded-3xl h-[35px] w-[15%] border-2 border-mid-blue hover:text-white hover:bg-mid-blue'>
+              <button className='rounded-3xl h-[35px] w-[100px] border-2 border-mid-blue hover:text-white hover:bg-mid-blue'>
                 Download
               </button>
             </div>
