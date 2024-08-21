ab673a8d968181133e524cecf876efb820c6be16
lifewithwendy <2022cs048@stu.ucsc.cmb.ac.lk>
2024-08-21 12:57:37 +0530

    create: sub controller and route

---
+++ api/routes/sub.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { verifyToken } from '../utils/verifyUser.js';
+import { 
+    makepayment, 
+    getsubs, 
+    deletesub, 
+    updatesub, 
+    payhere
+} from '../controllers/sub.controller.js';
+
+const router = express.Router();
+
+router.post('/makepayment', makepayment);
+router.post('/payhere', payhere);
+router.get('/getsubs', getsubs);
+router.delete('/deletesub/:subId', verifyToken, deletesub);
+router.put('/updatesub/:subId', verifyToken, updatesub);
+
+
+export default router;
