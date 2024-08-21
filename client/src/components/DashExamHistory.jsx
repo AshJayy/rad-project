fdfcbe1ba019ae65cdceb405c707d15266bd13e2
AthharPro <athharasif2002@gmail.com>
2024-08-21 14:16:34 +0530

    fix: ri incons to oi icons

--- client/src/components/DashExamHistory.jsx
+++ client/src/components/DashExamHistory.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Button, Spinner } from "flowbite-react";
-import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
+// import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 export default function DashExamHistory() {
   const [openedExams, setOpenedExams] = useState([]);
@@ -102,9 +103,11 @@ export default function DashExamHistory() {
             <div>{exam.name}</div>
             <div className='scale-[200%] mt-1'>
               {openedExams.includes(exam._id) ? (
-                <RiArrowDropUpLine />
+                // <RiArrowDropUpLine />
+                <IoMdArrowDropup />
               ) : (
-                <RiArrowDropDownLine />
+                // <RiArrowDropDownLine />
+                <IoMdArrowDropdown />
               )}
             </div>
           </span>
