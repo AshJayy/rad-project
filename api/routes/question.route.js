fd23bacd61f096b3b72bc13e12e85e8a55ed81cd
AshJayy <ashwinie.ja@gmail.com>
2024-09-01 19:54:34 +0530

    fix: added delete question route

--- api/routes/question.route.js
+++ api/routes/question.route.js
@@ -19,12 +19,8 @@ router.get("/getuserquestions", verifyToken, getUserQuestions);
 router.get("/getnextexam", verifyToken, getNextExam);
 router.get("/getquestions", verifyToken, getQuestions);
 router.put("/editQuestion/:questionId", verifyToken, editQuestion);
-router.delete("/deletequestion/:questionId", verifyToken, deleteQuestion);
 router.post('/create', verifyToken, createQuestion);
-router.get('/freetrial',verifyToken, getFreeTrial);
-router.get('/getquestions',verifyToken ,getQuestions);
-router.put('/editquestion', verifyToken,editQuestion);
-router.delete('/deletequestion/:questionId',verifyToken, deleteQuestion);
 router.get('/getquestion/:questionId',verifyToken, getQuestionById);
+router.delete('/deletequestion/:questionId', verifyToken, deleteQuestion);
 
 export default router;
