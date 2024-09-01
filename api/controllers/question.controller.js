3e4528d5613ed0877b6ee689899ea54ca7136886
ThuminduSena <140165904+ThuminduSena@users.noreply.github.com>
2024-09-01 13:32:49 +0530

    fix: getUsrcontroller

--- api/controllers/question.controller.js
+++ api/controllers/question.controller.js
@@ -66,14 +66,15 @@ export const getFreeTrial = async (req, res, next) => {
 
 export const getUserQuestions = async (req, res, next) => {
    try {
-      //get the recent exam details
+      // Get the recent exam details
       const pastExams = await Exam.find({ userID: req.user._id }).select('questions').lean();
 
-      //get the already answered Question ids
+      // Get the already answered Question IDs
       const usedQuestionIds = pastExams.reduce((acc, exam) => {
          return acc.concat(exam.questions.map(q => q.toString()));
       }, []);
 
+      // Function to get questions from a specific bank, excluding used questions
       const getFromBank = async (bank, limit) => {
          const questionSet = await Question.aggregate([
             { $match: { bank: bank, isActive: true, _id: { $nin: usedQuestionIds } } }, // Exclude used questions
@@ -91,16 +92,6 @@ export const getUserQuestions = async (req, res, next) => {
          getFromBank(5, 4),
          getFromBank(6, 4),
       ]);
-      //limits of the questions picked are in the rtio of 1:5 from the supplied requ to original requirments given
-
-      //bank    | given | original |
-      //  1     |   8   |    40    |
-      //  2     |   6   |    30    |
-      //  3     |   4   |    20    |
-      //  4     |   4   |    20    |
-      //  5     |   4   |    20    |
-      //  6     |   4   |    20    |
-      // Total  |  30   |   150    |
 
       // Combine all question sets into a single array
       const questions = questionSets.flat();
@@ -113,38 +104,10 @@ export const getUserQuestions = async (req, res, next) => {
 
    } catch (error) {
       console.error('Error fetching questions:', error);
-      next(error);
-   }
-   try {
-      const getFromBank = async (bank, limit) => {
-         const questionSet = await Question.aggregate([
-                     { $match: { bank: bank, isActive: true } }, // Exclude used questions
-                     { $sample: { size: limit } } // Randomly select the specified number of questions
-                  ]);
-         return questionSet
-      }
-
-      var questions = []
-
-      const questionSets = await Promise.all([
-         getFromBank(1, 8),
-         getFromBank(2, 6),
-         getFromBank(3, 4),
-         getFromBank(4, 4),
-         getFromBank(5, 4),
-         getFromBank(6, 4),
-      ]);
-
-      questionSets.forEach(set => {
-         questions = questions.concat(set);
-      });
-
-      res.status(200).json(questions);
-
-   } catch (error) {
-      next(error)
+      next(error); // Pass the error to the next middleware
    }
 }
+
 export const getNextExam = async (req, res, next) => {
    try {
      // Get the recent exam details for the user
