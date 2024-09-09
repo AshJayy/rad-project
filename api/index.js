4bd8037d0c276817fe1343d4c1a85a6c6529fab0
AshJayy <ashwinie.ja@gmail.com>
2024-09-09 14:29:53 +0530

    fix: build scripts

--- api/index.js
+++ api/index.js
@@ -39,8 +39,8 @@ app.use('/api/sub', subRoutes);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 //middleware 
 app.use((err, req, res, next) => {
