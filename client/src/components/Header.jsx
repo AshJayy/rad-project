2472f0ad9e03510009620383c39f2e8d8981e735
Nip <117910174+Kulath1@users.noreply.github.com>
2024-09-09 13:17:59 +0530

    style: UI fixes and responsive components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,32 +8,21 @@ import { FaSpinner } from "react-icons/fa";
 
 export default function Header() {
   const dropDownItems = [
-    {
-      name: "Product 1",
-      path: "#",
-    },
-    {
-      name: "Product 2",
-      path: "#",
-    },
-    {
-      name: "Product 3",
-      path: "#",
-    },
+    { name: "Product 1", path: "#" },
+    { name: "Product 2", path: "#" },
+    { name: "Product 3", path: "#" },
   ];
 
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
   const currentPath = location.pathname;
-  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const { loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
 
   const handleSignout = async () => {
     try {
-      const res = await fetch("/api/users/signout", {
-        method: "POST",
-      });
+      const res = await fetch("/api/users/signout", { method: "POST" });
       const data = await res.json();
       if (!res.ok) {
         console.log(data.message);
@@ -54,31 +43,17 @@ export default function Header() {
   };
 
   return (
-    <Navbar className="px-10 py-6 sticky top-0 z-10 backdrop-blur-lg bg-opacity-70">
-      <div className="flex gap-16 items-center">
-        <Logo />
+    <Navbar className="px-4 py-4 lg:px-10 lg:py-6 sticky top-0 z-10 backdrop-blur-lg bg-opacity-70">
+      <div className="flex items-center w-full">
+        {/* Logo */}
+        <div className="flex items-center">
+          <Logo />
+        </div>
 
-        <div className="flex gap-8 ">
-          <Dropdown
-            label=""
-            dismissOnClick={false}
-            renderTrigger={() => (
-              <span className="flex items-center gap-2 text-sm font-medium cursor-pointer text-dark-blue hover:text-mid-blue">
-                Products
-                <HiChevronDown />
-              </span>
-            )}
-          >
-            {dropDownItems.map((item, index) => (
-              <Link key={index} to={item.path}>
-                <Dropdown.Item>{item.name}</Dropdown.Item>
-              </Link>
-            ))}
-          </Dropdown>
-
-          <Navbar.Toggle />
-
+        <div className="hidden lg:flex items-center gap-8 ml-8">
           <Navbar.Collapse>
+            
+
             <Navbar.Link as={"div"}>
               <Link
                 to={"/pricing"}
@@ -87,6 +62,7 @@ export default function Header() {
                 Pricing
               </Link>
             </Navbar.Link>
+
             <Navbar.Link as={"div"}>
               <Link
                 to={"/about"}
@@ -97,95 +73,126 @@ export default function Header() {
             </Navbar.Link>
           </Navbar.Collapse>
         </div>
+
+        <Navbar.Toggle />
+
+        <div className="flex gap-4 items-center text-sm lg:flex-nowrap ml-auto">
+          {currentUser ? (
+            <>
+              <Dropdown
+                arrowIcon={false}
+                inline
+                label={
+                  <>
+                    <span className="block text-sm mr-2">
+                      Hi, {currentUser.username}
+                    </span>
+                    <Avatar
+                      alt="user"
+                      img={currentUser.profilePicture}
+                      rounded
+                      className="border-blue-500 w-10 h-10 lg:w-12 lg:h-12"
+                    />
+                  </>
+                }
+              >
+                <Dropdown.Header>
+                  <span className="block text-sm font-medium truncate">
+                    {currentUser.email}
+                  </span>
+                </Dropdown.Header>
+                {currentUser.userLevel >= 0 && (
+                  <>
+                    <Link to={"/dashboard?tab=dash"}>
+                      <Dropdown.Item>Dashboard</Dropdown.Item>
+                    </Link>
+                    <Dropdown.Divider />
+                  </>
+                )}
+                <Link to={"/dashboard?tab=profile"}>
+                  <Dropdown.Item>Profile</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
+              </Dropdown>
+            </>
+          ) : (
+            <span className="flex items-center">
+              {loading ? (
+                <>
+                  <FaSpinner className="animate-spin mr-2" />
+                  Loading...
+                </>
+              ) : (
+                <Link
+                  to={"/signin"}
+                  className="text-sm font-medium text-dark-blue hover:text-mid-blue"
+                >
+                  Log In
+                </Link>
+              )}
+            </span>
+          )}
+
+          <div className="hidden lg:block">
+            {currentPath !== "/freetrial" && (
+              <>
+                {currentUser ? (
+                  currentUser.userLevel === 0 &&
+                  currentUser.userTier === 0 && (
+                    <Button
+                      className="bg-mid-blue"
+                      pill
+                      onClick={handleFreeTrialClick}
+                      disabled={loading}
+                    >
+                      {loading ? (
+                        <>
+                          <FaSpinner className="animate-spin mr-2" />
+                          Loading...
+                        </>
+                      ) : (
+                        "Start free trial"
+                      )}
+                    </Button>
+                  )
+                ) : (
+                  <Button className="bg-mid-blue" pill>
+                    <Link to={"/signin"} state={{ from: "/freetrial" }}>
+                      Start free trial
+                    </Link>
+                  </Button>
+                )}
+              </>
+            )}
+          </div>
+        </div>
       </div>
 
-      <div className="flex gap-4 items-center text-sm">
-        {currentUser ? (
-          <>
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={
-                <>
-                  <span className="block text-sm mr-2">
-                    Hi, {currentUser.username}
-                  </span>
-                  <Avatar
-                    alt="user"
-                    img={currentUser.profilePicture}
-                    rounded
-                    className="border-blue-500"
-                  />
-                </>
-              }
-            >
-              <Dropdown.Header>
-                <span className="block text-sm font-medium truncate">
-                  {currentUser.email}
-                </span>
-              </Dropdown.Header>
-              {currentUser.userLevel >= 0 && (
-                <>
-                  <Link to={"/dashboard?tab=dash"}>
-                    <Dropdown.Item>Dashboard</Dropdown.Item>
-                  </Link>
-                  <Dropdown.Divider />
-                </>
-              )}
-              <Link to={"/dashboard?tab=profile"}>
-                <Dropdown.Item>Profile</Dropdown.Item>
-              </Link>
-              <Dropdown.Divider />
-              <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
-            </Dropdown>
-          </>
-        ) : (
-          <span className="flex items-center">
-            {loading ? (
-              <>
-                <FaSpinner className="animate-spin mr-2" />
-                Loading...
-              </>
-            ) : (
-              <Link
-                to={"/signin"}
-                className="text-sm font-medium text-dark-blue hover:text-mid-blue"
-              >
-                Log In
-              </Link>
-            )}
-          </span>
-        )}
+      <div className="lg:hidden">
+        <Navbar.Collapse>
 
-        {currentPath !== "/freetrial" && (
-          <>
-            {currentUser ? (
-              currentUser.userLevel === 0 && (currentUser.userTier === 0 && (
-                <Button
-                  className="bg-mid-blue"
-                  pill
-                  onClick={handleFreeTrialClick}
-                  disabled={loading}
-                >
-                  {loading ? (
-                    <>
-                      <FaSpinner className="animate-spin mr-2" />
-                      Loading...
-                    </>
-                  ) : (
-                    "Start free trial"
-                  )}
-                </Button>
-              ))
-            ) : (
-              <Button className="bg-mid-blue" pill>
-                <Link to={"/signin"} state={{ from: "/freetrial" }}>
-                  Start free trial
-                </Link>
-              </Button>
-            )}
-          </>
-        )}
+
+          {/* Pricing link */}
+          <Navbar.Link as={"div"}>
+            <Link
+              to={"/pricing"}
+              className="block mt-4 text-sm font-medium text-dark-blue hover:text-mid-blue"
+            >
+              Pricing
+            </Link>
+          </Navbar.Link>
+
+          {/* About link */}
+          <Navbar.Link as={"div"}>
+            <Link
+              to={"/about"}
+              className="block mt-4 text-sm font-medium text-dark-blue hover:text-mid-blue"
+            >
+              About
+            </Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </div>
     </Navbar>
   );
